02f13caf9ba5f52f0dd6bdb5db89062738f39d16
thiagoodev <cthiagoodev@gmail.com>
2025-07-03 21:50:06 -0300

    alteracoes

---
+++ src/br/com/thiagoodev/designpatterns/factory/kotlin/WindowButton.kt
@@ -0,0 +1,11 @@
+package br.com.thiagoodev.designpatterns.factory.kotlin
+
+class WindowButton : Button {
+    override fun render() {
+        TODO("Not yet implemented")
+    }
+
+    override fun onClick(action: () -> Unit) {
+        TODO("Not yet implemented")
+    }
+}
